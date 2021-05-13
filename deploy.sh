083da994ac9e2c3128888959db01eea7750d7e90
Keil Hunsaker <keil.hunsaker@global-e.com>
2021-05-13 15:38:21 -0400

    Update for Magento 2.4

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
-docker exec php_server chmod 777 -R /app/public
-docker exec -w /app/public php_server php -d memory_limit=-1 bin/magento cache:clean
-docker exec php_server chmod 777 -R /app/public
-docker exec -w /app/public php_server php -d memory_limit=-1 bin/magento setup:upgrade
-docker exec php_server chmod 777 -R /app/public
-docker exec -w /app/public php_server php -d memory_limit=-1 bin/magento setup:di:compile
-docker exec php_server chmod 777 -R /app/public
-docker exec -w /app/public php_server php -d memory_limit=-1 bin/magento setup:static-content:deploy -f
-docker exec php_server chmod 777 -R /app/public
+source ./.env
+docker exec php_server-$APP_EXT chmod 777 -R /app/public
+docker exec -w /app/public php_server-$APP_EXT php -d memory_limit=-1 bin/magento cache:clean
+docker exec php_server-$APP_EXT chmod 777 -R /app/public
+docker exec -w /app/public php_server-$APP_EXT php -d memory_limit=-1 bin/magento setup:upgrade
+docker exec php_server-$APP_EXT chmod 777 -R /app/public
+docker exec -w /app/public php_server-$APP_EXT php -d memory_limit=-1 bin/magento setup:di:compile
+docker exec php_server-$APP_EXT chmod 777 -R /app/public
+docker exec -w /app/public php_server-$APP_EXT php -d memory_limit=-1 bin/magento setup:static-content:deploy -f
+docker exec php_server-$APP_EXT chmod 777 -R /app/public
